a179265cdfe8ed095c9b1ff3416da5babd488877
lieby weintraub <liebahinda@gmail.com>
2019-03-27 16:37:21 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,8 +8,8 @@ def create_project_hash
  
 kickstarter = Nokogiri::HTML(html)
 # the selctor for projects: kickstarter.css("li.project.grid_4")
-#the selector for the title: 
+#the selector for the title: project.css("h2.bbcard_name strong a").text
 binding.pry 
-#project= {} 
+#projects= {} 
 end
 create_project_hash

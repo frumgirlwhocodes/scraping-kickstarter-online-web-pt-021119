664cf5e57c3b1f18d7919829f644271795166d84
lieby weintraub <liebahinda@gmail.com>
2019-03-27 17:44:50 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -17,8 +17,8 @@ kickstarter = Nokogiri::HTML(html)
  
 projects= {} 
  kickstarter.css("li.project.grid_4").each do |project| 
-   title= 
-   projects[={} 
+   title=  project.css("h2.bbcard_name strong a").text
+   projects[title.to_sym]={} 
  end 
  projects 
  end 
